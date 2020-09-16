f89aac30c644a44324336cb02791cda3b1b33cae
Surkin <surkin-ant@mail.ru>
2020-09-17 00:00:35 +0400

    Added class Person and PersonTest

---
+++ Practice-SAS/src/main/java/ru/ssau/tk/Practice_SAS/Person.java
@@ -0,0 +1,43 @@
+package ru.ssau.tk.Practice_SAS;
+
+public class Person {
+    private String firstName, lastName;
+    private int passportId;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(int passportId) {
+        this.passportId = passportId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+
+}
---
+++ Practice-SAS/src/test/java/ru/ssau/tk/Practice_SAS/PersonTest.java
@@ -0,0 +1,28 @@
+package ru.ssau.tk.Practice_SAS;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PersonTest {
+
+    private final static double DELTA = 0.00001;
+
+    @Test
+    public void testMethods() {
+        Person firstPerson = new Person();
+        Person secondPerson = new Person();
+        firstPerson.setFirstName("Jotaro");
+        secondPerson.setFirstName("Dio");
+        firstPerson.setLastName("Kujo");
+        secondPerson.setLastName("Brando");
+        firstPerson.setPassportId(91374682);
+        secondPerson.setPassportId(33333333);
+        assertEquals(firstPerson.getFirstName(), "Jotaro");
+        assertEquals(secondPerson.getFirstName(), "Dio");
+        assertEquals(firstPerson.getLastName(), "Kujo");
+        assertEquals(secondPerson.getLastName(), "Brando");
+        assertEquals(firstPerson.getPassportId(), 91374682, DELTA);
+        assertEquals(secondPerson.getPassportId(), 33333333, DELTA);
+    }
+}
