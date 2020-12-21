7a2393160805942aa621a2509b7923530ed6a34c
Surkin <surkin-ant@mail.ru>
2020-12-21 12:08:37 +0400

    Практика 1.3

---
+++ Practice-SAS/src/main/java/ru/ssau/tk/Practice_SAS/Point.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.Practice_SAS;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
