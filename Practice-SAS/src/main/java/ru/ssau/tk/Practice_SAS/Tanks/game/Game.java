13e7d17b65c08b59eec133bdaf5dc4940491999d
Surkin <surkin-ant@mail.ru>
2020-12-28 22:50:06 +0400

    Прописан update 60 раз в секунду

--- Practice-SAS/src/main/java/ru/ssau/tk/Practice_SAS/Tanks/game/Game.java
+++ Practice-SAS/src/main/java/ru/ssau/tk/Practice_SAS/Tanks/game/Game.java
@@ -1,6 +1,9 @@
 package ru.ssau.tk.Practice_SAS.Tanks.game;
 
-public class Game implements Runnable{
+import ru.ssau.tk.Practice_SAS.Tanks.display.Display;
+import ru.ssau.tk.Practice_SAS.Tanks.utils.Time;
+
+public class Game implements Runnable {
 
     public static final int WIGHT = 800;
     public static final int HEIGHT = 600;
@@ -9,33 +12,86 @@ public class Game implements Runnable{
     public static final int NUM_BUFFERS = 3;
 
     public static final float UPDATE_RATE = 60.0f;
+    public static final float UPDATE_INTERVAL = Time.SECOND / UPDATE_RATE;
+    public static final long IDLE_TIME = 1;
 
-    public Game(){
+    private boolean running;
+    private Thread gameThread;
+
+    public Game() {
+        running = false;
+        Display.create(WIGHT, HEIGHT, TITLE, CLEAR_COLOR, NUM_BUFFERS);
+    }
+
+    public synchronized void start() {
+
+        if (running)
+            return;
+
+        running = true;
+        gameThread = new Thread(this);
+        gameThread.start();
 
     }
 
-    public synchronized void start(){
+    public synchronized void stop() {
+
+        if (!running)
+            return;
+
+        running = false;
+
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        cleanup();
+    }
+
+    private void update() {
 
     }
 
-    public synchronized void stop(){
+    private void render() {
 
     }
 
-    private void update(){
+    public void run() {
+
+        float delta = 0;
+
+        long lastTime = Time.get();
+        while (running) {
+            long now = Time.get();
+            long elapsedTime = now - lastTime;
+            lastTime = now;
+
+            boolean render = false;
+            delta += (elapsedTime / UPDATE_INTERVAL);
+            while (delta > 1) {
+                update();
+                delta--;
+                render = true;
+            }
+            if (render)
+                render();
+            else {
+                try {
+                    Thread.sleep(IDLE_TIME);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
 
     }
 
-    private void render(){
-
-    }
-
-    public void run(){
-
-    }
-
-    private void cleanup(){
-
+    private void cleanup() {
+        Display.destroy();
     }
 
 }
